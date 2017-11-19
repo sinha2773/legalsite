bc0687fe6ee2c137b5de1766959a64d34e160821
dolarst <dolarcse123@gmail.com>
2017-11-19 16:01:38 +0600

    some pages added

---
+++ wp-content/themes/southside/blog_template.php
@@ -0,0 +1,112 @@
+<?php 
+
+  // Template Name: Blog Template
+  get_header();
+ ?>
+<?php 
+        $p = new WP_Query(array(
+            'post_type' => 'page',
+            'posts_per_page' => 1,
+            'page_id' =>76
+        ));
+    ?>
+    <?php 
+        if($p->have_posts()): 
+        while($p->have_posts()): 
+        $p->the_post();  
+    ?>
+        <section class="common_banner_section">
+            <div class="common_banner_images">
+              <?php the_post_thumbnail('testimonial-bg-image'); ?>
+            </div>
+        </section>
+    <?php endwhile; endif; ?>  
+ <section class="blog_section">
+   <div class="container">
+     <div class="main_blog_div">
+       <div class="blog_content">
+         <div class="blog_header">
+           <div class="row">
+             <div class="col-md-12">
+               <div class="blog_header_content">
+                  <h2><?php the_title(); ?></h2>
+                                  
+               </div>
+             </div>
+           </div>
+         </div>
+         <div class="blog_body">
+           <div class="row">
+             <div class="col-md-8">
+               <div class="blog_post_list">
+                 <div class="row">
+                  <?php
+                    $paged = get_query_var('paged') ? get_query_var('paged') : 1; 
+                      $all_blog_post = new WP_Query(array(
+                        'post_type' => 'post',
+                        "paged" => $paged,
+                        'posts_per_page' => 9
+                        
+                    ));
+                  ?>
+                  <?php 
+                    if($all_blog_post ->have_posts()): 
+                    while($all_blog_post->have_posts()) :
+                     $all_blog_post->the_post(); 
+                  ?>
+                   <div class="col-md-4">
+                     <div class="blog_post_item">
+                       <div class="blog_post_item_images">
+                         <?php the_post_thumbnail('blolg_image'); ?>
+                       </div>
+                       <div class="blog_post_item_images_text">
+                         <div class="blog_post_item_images_text_title">
+                           <h3><?php the_title(); ?></h3>
+                         </div>
+                         <div class="blog_post_item_images_text_body">
+                           <p><?php echo get_trim_content(25,true); ?></p>
+                           
+                         </div>
+                         <div class="blog_post_item_images_text_body_details">
+                           <a class="blog_post_a_tag" href="<?php the_permalink(); ?>">Details</a>
+                         </div>
+                         <div class="blog_post_item_images_text_date_time">
+                           <span><?php echo get_the_date(); ?></span>
+                           <span class="blog_post_date"><?php echo get_the_time(); ?></span>
+                         </div>
+                       </div>
+                     </div>
+                   </div>
+                 <?php endwhile; endif; ?>
+                 
+                 </div>
+                 <div class="main-pagination pull-right">
+                  <?php 
+                  if(function_exists('wp_pagenavi')) { wp_pagenavi( array( 'query' => $all_blog_post )); }  
+                  // if(function_exists('wp_pagenavi')) { wp_pagenavi(); }  
+                  ?>
+              </div>
+               </div>
+               
+             </div>
+            
+             <div class="col-md-4">
+               <div class="blog_right_sidebar">
+                 
+                 <div class="blog_right_sidebar_content">
+                   <?php dynamic_sidebar('blog_right_sidebar'); ?>
+                 </div>
+                 
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+ </section>
+
+
+<?php 
+  get_footer();
+ ?>
---
+++ wp-content/themes/southside/faq_template.php
@@ -0,0 +1,71 @@
+<?php 
+ get_header();
+  // Template Name: Faq
+?>
+<?php 
+        $p = new WP_Query(array(
+            'post_type' => 'page',
+            'posts_per_page' => 1,
+            'page_id' =>186
+        ));
+    ?>
+    <?php 
+        if($p->have_posts()): 
+        while($p->have_posts()): 
+        $p->the_post();  
+    ?>
+        <section class="common_banner_section">
+            <div class="common_banner_images">
+              <?php the_post_thumbnail('testimonial-bg-image'); ?>
+            </div>
+        </section>
+    <?php endwhile; endif; ?>       
+        <section class="what_we_do_section">
+            <div class="container">
+              <div class="what_we_do_main">
+                <div class="row">
+                  <div class="col-md-4">
+                        <?php get_sidebar(); ?>
+                    </div>
+                    <div class="col-md-8">                      
+                      <div class="faq_information">
+                        <div id="accordion" role="tablist" aria-multiselectable="true">
+                          <?php 
+                              $faq_store = new WP_Query(array(
+                                  'post_type' => 'faq',
+                                  'posts_per_page' => -1,
+                                  
+                              ));
+                          ?>
+                          <?php 
+                              $i=20; 
+                              if($faq_store->have_posts()): 
+                              while($faq_store->have_posts()):
+                              $faq_store->the_post();
+                               
+                          ?>
+                          <div class="card">
+                            <div class="card-header faq_header" role="tab" id="heading<?php echo $i; ?>">
+                              <h5 class="mb-0" class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo $i; ?>" aria-expanded="false" aria-controls="collapse<?php echo $i; ?>">
+                                  <i class="fa fa-plus" aria-hidden="true"></i>   
+                                  <i class="fa fa-minus" aria-hidden="true" style="display: none;"></i>   
+                                <?php the_title(); ?>
+                              </h5>
+                               <div id="collapse<?php echo $i; ?>" class="collapse" role="tabpanel" aria-labelledby="heading<?php echo $i; ?>">
+                                <div class="card-block faq_content">
+                                  <?php the_content(); ?>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <?php $i++; endwhile; endif; ?>
+                        </div>
+                      </div>
+                    </div>                    
+                </div>
+              </div>
+            </div>
+        </section>
+<?php 
+  get_footer();
+?>
